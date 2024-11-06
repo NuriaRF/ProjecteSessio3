eaaee7ae9e9def0e8f55a92849f642b7f2145e17
Nuria <nuria.ruiz@institutmetropoli.cat>
2024-11-06 09:22:26 +0100

    Initial commit

---
+++ Codi_amb_control_de_versions.R
@@ -0,0 +1,14 @@
+# 1.Primer hem fet a R:
+
+library("usethis")
+use_git_config(user.name = "Nuria", user.email = "nuria.ruiz@institutmetropoli.cat")
+# user.name no cal que sigui el meu usuari, només ha de ser informatiu per mi i per la resta
+# el e-mail sí ha de ser el del meu compte
+
+usethis::git_default_branch_configure()
+
+
+
+
+# 2.Hem creat un nou projecte a la carpeta on treballarem i viurà el codi de R
+#IMPORTANT No pot ser dintre d'una subcarpeta, sino a la carpeta on viu el projecte viu el codi 
