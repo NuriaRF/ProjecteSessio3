e31878b365bc1013c5cffdc2832ab282da2a64ae
Nuria <nuria.ruiz@institutmetropoli.cat>
2024-11-06 09:50:39 +0100

    He creat un Script que diu Bon dia i Benvinguts a la sessió 3

--- Codi_amb_control_de_versions.R
+++ Codi_amb_control_de_versions.R
@@ -11,4 +11,10 @@ usethis::git_default_branch_configure()
 
 
 # 2.Hem creat un nou projecte a la carpeta on treballarem i viurà el codi de R
-#IMPORTANT No pot ser dintre d'una subcarpeta, sino a la carpeta on viu el projecte viu el codi 
+# IMPORTANT No pot ser dintre d'una subcarpeta, sino a la carpeta on viu el projecte viu el codi
+
+
+
+# 3. Creem un projecte a RStudio amb control de versions
+use_git()
+# ens demana si volem commit els canvis. li direm que sí i ens reinicia el RStudio i ens apareix la pestanyeta Git al visor de l'Environment
---
+++ Script_a_compartir.R
@@ -0,0 +1,4 @@
+# NRF: Creo Script per compartir. 
+
+print("Bon dia a tothom")
+print("Benvinguts a la sessió 3")
